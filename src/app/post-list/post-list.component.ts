ce86632cbd1edd87f8723d90f04e1181b56b4a95
augustin <augustin_kave_08@outlook.fr>
2019-05-10 15:46:43 +0200

    First step title + button ok with property binding

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
+  firstPost = 'Mon premier post';
+  twoPost = 'Mon deuxième post';
+  threePost = 'Mon troisième post';
   constructor() { }
 
   ngOnInit() {
